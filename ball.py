0434d29269e5885eab2d5e0c3ec18dae46937317
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-17 21:21:55 -0500

    Added ball object.

---
+++ ball.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+class Ball:
+
+    def __init__(self):
+        ball = Turtle(shape= "circle")
+        ball.color("white")
+        ball.penup()
+        ball.shapesize(stretch_wid=.5)
+
+    
