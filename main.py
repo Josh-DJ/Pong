14112b6c4bfb29c27fbb7451f08ec290ebab3987
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-17 21:21:17 -0500

    Added paddles to game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,11 +12,14 @@ screen.title("Pong")
 screen.tracer(0)
 
 
-paddle = Paddle()
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 
 screen.listen()
-screen.onkeypress(key="Up", fun=paddle.up)
-screen.onkeypress(key="Down", fun=paddle.down)
+screen.onkeypress(key="Up", fun=l_paddle.up)
+screen.onkeypress(key="Down", fun=l_paddle.down)
+screen.onkeypress(key="w", fun=r_paddle.up)
+screen.onkeypress(key="s", fun=r_paddle.down)
 
 game_on = True
 while game_on:
