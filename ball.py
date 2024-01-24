ba068451b2b75a64c74702165e7f444d92989c04
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-23 22:02:11 -0500

    Implemented ball speed functionality.

--- ball.py
+++ ball.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 class Ball(Turtle):
 
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -11,6 +12,7 @@ class Ball(Turtle):
         self.goto(0,0)
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     
     def move(self):
@@ -23,8 +25,10 @@ class Ball(Turtle):
 
     def hit(self):
         self.x_move = self.x_move * -1
+        self.move_speed *= .9
 
     def reset_position(self):
         self.goto(0,0)
+        self.move_speed = 0.1
         self.hit()
 
