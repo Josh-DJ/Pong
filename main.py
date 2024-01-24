5c522625ce9b239f21af3fbd51e6a5fad1c40e91
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-23 21:30:31 -0500

    Added collision detection with walls and paddles.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -14,6 +15,7 @@ screen.tracer(0)
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkeypress(key="Up", fun=l_paddle.up)
@@ -23,6 +25,28 @@ screen.onkeypress(key="s", fun=r_paddle.down)
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    # Collision with top/bottom wall
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        #Bounce ball
+        ball.bounce()
+
+    # Collision with paddle. Checks distance away from paddle and also x coord of ball from paddle.
+    if (ball.distance(l_paddle) < 30 and ball.xcor() < -330) or (ball.distance(r_paddle) < 30 and ball.xcor() > 330):
+        #Paddle hit
+        ball.hit()
+
+    # Collision with left/right walls
+    if ball.xcor() > 390:
+        # Left paddle gets a point
+        l_paddle.score += 1
+        ball.reset_position()
+
+    elif ball.xcor() < -390:
+        # Right paddle gets a point
+        r_paddle.score += 1
+        ball.reset_position()
 
 screen.exitonclick()
