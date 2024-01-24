8533c8b018e9d307d3af7b34e0a64bdd744371c7
Joshua Jimenez <jjimen7m20@gmail.com>
2024-01-23 22:02:45 -0500

    Added scoreboard class and functionality.

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+
+    def update_point(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 60, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 60, "normal"))
+
+    def l_point(self, score):
+        self.l_score = score
+    def r_point(self, score):
+        self.r_score = score
